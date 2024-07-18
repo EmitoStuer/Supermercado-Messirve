dce2d031f2bd349000d2fe71e7781c54af597978
EmitoStuer <stuer.87@gmail.com>
2024-07-17 22:08:28 -0300

    Probando Js

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+/*let ofertasNombres = new Array("Promo Cafecito", "Promo Almuerzo","Promo Merienda");
+
+let ofertasDescripcion = new Array("Leche en polvo + Cafe Instantaneo x2 + Azucar","Arroz + Fideos x2 + Polenta","Galletitas a Eleccion + Chocolatada x 2");
+
+let ofertasPrecios = new Array(10000,6000,7000);
+
+let ofertasImagenes = new Array("img1", "img2","img3");
+console.log("hola")
+for (i=0;i>ofertasNombres.length;i++){
+    console.log (ofertasNombres[i]);
+    console.log (ofertasDescripcion[i]);
+    console.log (ofertasPrecios[i]);
+    console.log (ofertasImagenes[i]);
+}
+*/
+
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click",accionar);
+
+function accionar(){
+    alert("Alert");
+
+}
+/*
+let texto = document.getElementById("texto");
+let contenedorOfertas = document.getElementById("ofertas");
+
+let nuevoElemento = document.createElement("h1");
+
+let textoElemento = document.createTextNode("soyun texto");
+
+contenedorOfertas.innertext = "<h1>Hola vengo desde Js</h1>";
+
+nuevoElemento.appendChild(textoElemento);
+
+contenedorOfertas.appendChild(nuevoElemento);
+
+texto.innertext = "Zarafaz";
+
+
+*/
