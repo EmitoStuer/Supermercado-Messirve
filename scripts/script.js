02b762c2d4161c7568eecf7474fea919b49abc8d
EmitoStuer <stuer.87@gmail.com>
2024-07-19 21:15:14 -0300

    Divs de Promos con Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,113 @@
-const ofertasNombres = ["Promo Cafecito", "Promo Almuerzo","Promo Merienda"]; 
+const ofertasNombres = ["Promo Cafecito", "Promo Almuerzo", "Promo Merienda"];
 
-const ofertasDescripcion = ["Leche en polvo + Cafe Instantaneo x2 + Azucar","Arroz + Fideos x2 + Polenta","Galletitas a Eleccion + Chocolatada x 2"];
+const ofertasDescripcion = ["Leche en polvo + Cafe Instantaneo x2 + Azucar", "Arroz + Fideos x2 + Polenta", "Galletitas a Eleccion + Chocolatada x 2"];
 
-const ofertasPrecios = [10000,6000,7000];
+const ofertasPrecios = [10000, 6000, 7000];
 
-const ofertasImagenes = ["img1", "img2","img3"];
+const ofertasImagenes = ["./imagenes/promoCafecito.jpg", "./imagenes/promoAlmuerzo.jpg", "./imagenes/promoMerienda.jpg"];
 
-console.log("hola")
-for (let i=0;i<ofertasNombres.length;i++){
-    console.log (ofertasNombres[i]);
-    console.log (ofertasDescripcion[i]);
-    console.log (ofertasPrecios[i]);
-    console.log (ofertasImagenes[i]);
+const descripcionImagenes = ["Imagen de Cafe", "Imagen de Plato de Comida", "Imagen de Medialunas"];
+
+for (let i = 0; i < ofertasNombres.length; i++) {
+    cargarOfertas(ofertasNombres[i], ofertasDescripcion[i], ofertasPrecios[i], ofertasImagenes[i],descripcionImagenes[i])
 }
 
+function cargarOfertas(nombre, descripcion, precio, imagen,descripcionImagen) {
+
+    let contenedorOfertas = document.getElementById("ofertas");
+
+    let nuevoElementoDiv = document.createElement("div");
+
+    let nuevoElementoImg = document.createElement("img");
+
+    nuevoElementoImg.setAttribute("class", "imagenesOfertas");
+    nuevoElementoImg.setAttribute("src", imagen);
+    nuevoElementoImg.setAttribute("alt", descripcionImagen);
 
 
-let btn = document.getElementById("btn");
-btn.addEventListener("click",accionar);
+    let nuevoElementoH1 = document.createElement("h2");
 
-function accionar(){
-    alert("Alert");
+    let textoElementoH1 = document.createTextNode(nombre);
+
+    let nuevoElementoH2 = document.createElement("p");
+
+    let textoElementoH2 = document.createTextNode(descripcion);
+
+    let nuevoElementoP = document.createElement("p");
+
+    let textoElementoP = document.createTextNode(`$ ${precio}`);
+
+    nuevoElementoDiv.appendChild(nuevoElementoImg);
+
+    contenedorOfertas.appendChild(nuevoElementoDiv);
+
+    nuevoElementoH1.appendChild(textoElementoH1);
+
+    nuevoElementoDiv.appendChild(nuevoElementoH1);
+
+    nuevoElementoDiv.setAttribute("class", "card");
+
+    contenedorOfertas.appendChild(nuevoElementoDiv);
+
+    nuevoElementoH2.appendChild(textoElementoH2);
+
+    nuevoElementoDiv.appendChild(nuevoElementoH2);
+
+    contenedorOfertas.appendChild(nuevoElementoDiv);
+
+    nuevoElementoP.appendChild(textoElementoP);
+
+    nuevoElementoDiv.appendChild(nuevoElementoP);
+
+    contenedorOfertas.appendChild(nuevoElementoDiv);
+
 
 }
 /*
-let texto = document.getElementById("texto");
 let contenedorOfertas = document.getElementById("ofertas");
 
-let nuevoElemento = document.createElement("h1");
+let nuevoElementoDiv = document.createElement("div");
 
-let textoElemento = document.createTextNode("soyun texto");
+let nuevoElementoImg = document.createElement("img");
 
-contenedorOfertas.innertext = "<h1>Hola vengo desde Js</h1>";
-
-nuevoElemento.appendChild(textoElemento);
-
-contenedorOfertas.appendChild(nuevoElemento);
-
-texto.innertext = "Zarafaz";
+nuevoElementoImg.setAttribute("class", "imagenesOfertas");
+nuevoElementoImg.setAttribute("src", ofertasImagenes[0]);
+nuevoElementoImg.setAttribute("alt", descripcionImagenes[0]);
 
 
+let nuevoElementoH1 = document.createElement("h1");
+
+let textoElementoH1 = document.createTextNode(ofertasNombres[0]);
+
+let nuevoElementoH2 = document.createElement("h2");
+
+let textoElementoH2 = document.createTextNode(ofertasDescripcion[0])
+
+let nuevoElementoP = document.createElement("p");
+
+let textoElementoP = document.createTextNode(`$ ${ofertasPrecios[0]}`);
+
+nuevoElementoDiv.appendChild(nuevoElementoImg);
+
+contenedorOfertas.appendChild(nuevoElementoDiv);
+
+nuevoElementoH1.appendChild(textoElementoH1);
+
+nuevoElementoDiv.appendChild(nuevoElementoH1);
+
+nuevoElementoDiv.setAttribute("class", "card");
+
+contenedorOfertas.appendChild(nuevoElementoDiv);
+
+nuevoElementoH2.appendChild(textoElementoH2);
+
+nuevoElementoDiv.appendChild(nuevoElementoH2);
+
+contenedorOfertas.appendChild(nuevoElementoDiv);
+
+nuevoElementoP.appendChild(textoElementoP);
+
+nuevoElementoDiv.appendChild(nuevoElementoP);
+
+contenedorOfertas.appendChild(nuevoElementoDiv);
 */
