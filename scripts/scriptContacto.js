f6449bb8da2e65848ae2ff64cce9c32cd12c649f
EmitoStuer <stuer.87@gmail.com>
2024-07-31 21:12:08 -0300

    Comprobacion de campos de form

--- scripts/scriptContacto.js
+++ scripts/scriptContacto.js
@@ -6,16 +6,34 @@ let cajaComentario = document.getElementById("comentario");
 let informacion = [];
 
 let btnEnviar = document.getElementById("enviar");
-btnEnviar.addEventListener("click", guardarDatos);
+btnEnviar.addEventListener("click", comprobarDatos);
 
 function guardarDatos () {
+
     informacion[0] = nombre.value;
     informacion[1] = apellido.value;
     informacion[2] = email.value;
     informacion[3] = number.value;
     informacion[4] = cajaComentario.value;
     console.log(informacion);
+
     let blob = new Blob ([informacion], { type: "text/plain;charset=utf-8" }) ;
 
     saveAs(blob, "contacto.txt");
 }
+
+function comprobarDatos(){
+    const inputs = document.querySelectorAll("#miFormulario input");
+    let camposCompletos = true;
+        for (let i=0; i<inputs.length;i++){
+            if (inputs[i].value==""){
+                camposCompletos = false;
+            }
+        }
+
+        if (camposCompletos){
+            guardarDatos();
+        }else{
+            alert("debe completar todos los campos");
+        }
+    }
